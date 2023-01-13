a1682f5303cd8387490076a0eaaee2c8c438a97f
Reza Vilera <vilera_reza99@yahoo.com>
2023-01-13 11:25:29 +0700

    update test inference

--- test_inference.py
+++ test_inference.py
@@ -1,24 +1,37 @@
 from pycoral.utils import edgetpu
 from pycoral.adapters import common
+import tflite_runtime.interpreter as tflite
 from PIL import Image
 import time
 import cv2 as cv
+import numpy as np
 
 model_file = 'models/vggface_quant.tflite'
 img_file = 'images/reza.jpg'
 
-interpreter = edgetpu.make_interpreter(model_file)
+# For pycoral
+#interpreter = edgetpu.make_interpreter(model_file)
+
+interpreter = tflite.Interpreter(model_file, experimental_delegates=[tflite.load_delegate('libedgetpu.so.1')])
 interpreter.allocate_tensors()
+interpreter_output = interpreter.get_output_details()[0]
+interpreter_input = interpreter.get_input_details()[0]
 
 
-size = common.input_size(interpreter)
+#size = common.input_size(interpreter)
 #img = Image.open(img_file).convert('RGB').resize(size, 1)
+
 img = cv.imread(img_file)[:,:,::-1]
+img = np.expand_dims(img, axis=0)
+# Predict vector
+# vector = self.classifier.predict(face)[0]
 
 for i in range(5):
-    print (size)
+    print (img.shape)
     t1 = time.time()
-    common.set_input(interpreter, img)
+    # for pycoral
+    # common.set_input(interpreter, img)
+    interpreter.set_tensor(interpreter['index'], img)
     interpreter.invoke()
 
     t2 = time.time()
