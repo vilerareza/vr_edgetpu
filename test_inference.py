39c730109437f74e70928e0f61e3a3cfd5465b93
Reza Vilera <vilera_reza99@yahoo.com>
2023-01-13 11:04:59 +0700

    update test inference

--- test_inference.py
+++ test_inference.py
@@ -9,10 +9,12 @@ img_file = 'images/reza.jpg'
 interpreter = edgetpu.make_interpreter(model_file)
 interpreter.allocate_tensors()
 
-t1 = time.time()
+
 size = common.input_size(interpreter)
 img = Image.open(img_file).convert('RGB').resize(size, 1)
 
+print (size)
+t1 = time.time()
 common.set_input(interpreter, img)
 interpreter.invoke()
 
