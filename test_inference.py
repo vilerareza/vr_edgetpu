259625182fdee5d9e11123cd0cfa1e4a96f389d5
Reza Vilera <vilera_reza99@yahoo.com>
2023-01-13 10:56:54 +0700

    update test inference

--- test_inference.py
+++ test_inference.py
@@ -1,6 +1,7 @@
 from pycoral.utils import edgetpu
 from pycoral.adapters import common
 from PIL import Image
+import time
 
 model_file = 'models/vggface_quant.tflite'
 img_file = 'images/reza.jpg'
@@ -8,6 +9,12 @@ img_file = 'images/reza.jpg'
 interpreter = edgetpu.make_interpreter(model_file)
 interpreter.allocate_tensors()
 
+t1 = time.time()
 size = common.input_size(interpreter)
 img = Image.open(img_file).convert('RGB').resize(size, 1)
 
+common.set_input(interpreter, img)
+interpreter.invoke()
+
+t2 = time.time()
+print ((t2-t1))
